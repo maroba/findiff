a49759839de2923929a42fc9061c40a76a1e8002
maroba <matthias.r.baer@googlemail.com>
2018-03-21 16:51:44 +0100

    Add code documentation

--- findiff/diff.py
+++ findiff/diff.py
@@ -3,8 +3,35 @@ from findiff.coefs import coefficients
 
 
 class FinDiff(object):
+    """Finite difference representation of a derivative of any order, any accuracy in any dimension"""
+
 
     def __init__(self, h=[1.], dims=[0], acc=2):
+        """Constructor for Finite Difference operator
+           
+           Parameters:
+           ----------
+           
+           h        array-like
+                    The grid spacing along each axis.
+           dims     array-like
+                    The axes along which to take the derivatives. Multiple values mean higher derivative.
+           acc      int
+                    The accuracy order of the finite difference scheme.
+                            
+                            
+           Example:
+           --------
+           
+           Suppose f is a four-dimensional array. The second partial derivative with respect to the second axis,
+                    
+                    \frac{\partial^2 f}{\partial z^2},
+                     
+           on a grid with equidistant spacing h=[0.1, 0.1, 0.1, 0.1] is given by
+           
+                FinDiff(h=[0.1, 0.1, 0.1, 0.1], dims=[1, 1])
+                
+        """
 
         if not hasattr(h, "__len__"):
             self._h = np.array([h])
@@ -24,6 +51,7 @@ class FinDiff(object):
             self._coefs.append(coefficients(self._derivs[i], acc))
 
     def diff(self, y):
+        """Applies the finite difference operator to a function y"""
 
         ndims = len(y.shape)
         if ndims != len(self._h):
@@ -36,6 +64,51 @@ class FinDiff(object):
                 yd = _diff_general(yd, self._h, self._derivs[i], i, self._acc, self._coefs[i])
 
         return yd
+
+
+class Laplacian(object):
+    """A representation of the Laplace operator in arbitrary dimensions using finite difference schemes"""
+
+    def __init__(self, h=[1.], acc=2):
+        """Constructor for the Laplacian
+        
+           Parameters:
+           -----------
+           
+           h        array-like
+                    The grid spacing along each axis
+           acc      int
+                    The accuracy order of the finite difference scheme        
+        """
+
+        if not hasattr(h, "__len__"):
+            h = np.array([h])
+        else:
+            h = np.array(h)
+
+        self._parts = [FinDiff(h, dims=[k, k], acc=acc) for k in range(len(h))]
+
+    def apply(self, f):
+        """Applies the Laplacian to the array f
+        
+           Parameters:
+           -----------
+           
+           f        ndarray
+                    The function to differentiate given as an array.
+        
+           Returns:
+           --------    
+           
+           an ndarray with Laplace(f)
+        
+        """
+        laplace_f = np.zeros_like(f)
+
+        for part in self._parts:
+            laplace_f += part.diff(f)
+
+        return laplace_f
 
 
 def diff(y, h, dims=[0], acc=2):
@@ -89,11 +162,11 @@ def _diff_general(y, h, deriv, dim, acc, coefs=None):
     if coefs is None:
         coefs = coefficients(deriv, acc)
 
+    npts = y.shape[dim]
+
     scheme = "center"
     weights = coefs[scheme]["coefficients"]
     offsets = coefs[scheme]["offsets"]
-
-    npts = y.shape[dim]
 
     nbndry = len(weights) // 2
     ref_slice = slice(nbndry, npts - nbndry, 1)
@@ -126,6 +199,7 @@ def _diff_general(y, h, deriv, dim, acc, coefs=None):
 
 
 def _apply_to_array(yd, y, weights, off_slices, ref_slice, dim):
+    """Applies the finite differences only to slices along a given axis"""
 
     ndims = len(y.shape)
 
