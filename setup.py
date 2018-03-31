bc13fe04b80b11c870e86b904d082652453125b3
maroba <matthias.r.baer@googlemail.com>
2018-03-31 12:31:48 +0200

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='findiff',
-    version='0.1.2',
+    version='0.2.0',
     description='A Python package for finite difference derivatives in any number of dimensions.',
     long_description='A Python package for finite difference derivatives in any number of dimensions.',
 
