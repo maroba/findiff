b8e6f38d654021c52302d321940971fea9b30780
maroba <matthias.r.baer@googlemail.com>
2018-09-12 20:08:39 +0200

    Tidy up

--- findiff/vector.py
+++ findiff/vector.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 from findiff.findiff import FinDiff
-from findiff.util import wrap_in_ndarray
 
 
 class VectorOperator(object):
@@ -261,3 +260,16 @@ class Laplacian(object):
 
         return laplace_f
 
+
+def wrap_in_ndarray(value):
+    """Wraps the argument in a numpy.ndarray.
+
+       If value is a scalar, it is converted in a list first.
+       If value is array-like, the shape is conserved.
+
+    """
+
+    if hasattr(value, "__len__"):
+        return np.array(value)
+    else:
+        return np.array([value])
