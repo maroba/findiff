4400da293f7d6f7dade88f6154dc495c4bf970d9
maroba <matthias.r.baer@googlemail.com>
2019-05-23 18:22:43 +0200

    Release 0.6.3

--- findiff/__init__.py
+++ findiff/__init__.py
@@ -2,4 +2,4 @@ from .coefs import coefficients
 from .operators import FinDiff, Coef, Identity, Coefficient
 from .vector import Gradient, Divergence, Curl, Laplacian
 
-__version__ = "0.6.1"
+__version__ = "0.6.3"
