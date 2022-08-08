392be0b0cf3a4ba3519e904a66e5d367c901d3dc
maroba <matthias.r.baer@googlemail.com>
2022-08-08 17:07:01 +0200

    Plot function on grid

--- docs/scripts/plt_func_on_grid.py
+++ docs/scripts/plt_func_on_grid.py
@@ -28,9 +28,16 @@ ax.set_yticks(ticks=[], labels=[])
 
 ax.set_ylim(0.6, 1.02)
 
-for xi, fi in zip(x, f):
+for i, (xi, fi, label) in enumerate(zip(x, f, labels)):
     line = Line2D((xi, xi), (0, fi), color="grey", linestyle="--")
     ax.add_line(line)
+    txt = ax.text(xi, fi+0.01, label.replace("x", "f"), horizontalalignment="right", verticalalignment="bottom")
+
+    if i >= 3:
+        txt.set_horizontalalignment("center")
+
+    if i >= 5:
+        txt.set_horizontalalignment("left")
 
 ax.plot(x_fine, f_fine, "-")
 ax.plot(x, f, 'o')
@@ -43,13 +50,16 @@ ax.text(x[1] + dx/2, f[1] - 0.1, r"$\Delta x$", horizontalalignment="center", ve
 ax.spines["right"].set_visible(False)
 ax.spines["top"].set_visible(False)
 
-ax.spines["bottom"].set_linestyle("->")
+print(ax.yaxis.get_view_interval())
+bottom = ax.yaxis.get_view_interval()[0]
+ax.plot(x, [bottom]*len(x), 'o', clip_on=False, color="C0")
 
+xax = ax.xaxis.get_view_interval()
+yax = ax.yaxis.get_view_interval()
 
-#ax.plot(ax.xaxis.get_view_interval()[1], f[0], ">k", clip_on=False)
-#ax.plot(0, 1, "^k", clip_on=False)
-
-plt.savefig("func_on_grid.png")
+arrow = FancyArrowPatch((xax[0], yax[0]), (xax[0], yax[1]), arrowstyle='-|>', mutation_scale=20, clip_on=True)
+ax.add_patch(arrow)
+plt.savefig("../source/images/func_on_grid.png")
 
 plt.show()
 
